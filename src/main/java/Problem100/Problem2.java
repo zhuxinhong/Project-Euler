4e01f123723e2507bb1715662c77c4c53f0c805e
zhuxh <zhuxinhong@bilibili.com>
2015-05-29 17:12:38 +0800

    2

---
+++ src/main/java/Problem100/Problem2.java
@@ -0,0 +1,33 @@
+package Problem100;
+
+/**
+ * Created by zhuxh on 15-5-29.
+ */
+public class Problem2 {
+
+    public static void main(String[] args) {
+
+        int sum = 0;
+
+        int sum_even = 0;
+
+        int a = 1;
+        int b = 2;
+
+        Boolean flag = true;
+        while(flag){
+            sum = a + b;
+            a = b;
+            b = sum;
+            if(b >= 4000000)
+                flag = false;
+
+            if(a % 2 == 0)
+                sum_even += a;
+
+        }
+
+        System.out.println(sum_even);
+
+    }
+}
