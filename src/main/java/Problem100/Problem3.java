9504506d978212875d0d2731a457e9aa2212d565
zxh900729@msn.cn <zxh3176005>
2015-05-30 00:35:37 +0800

    第三题

--- src/main/java/Problem100/Problem3.java
+++ src/main/java/Problem100/Problem3.java
@@ -5,25 +5,39 @@ package Problem100;
  */
 public class Problem3 {
 
-
+    //answer 6857
     public static void main(String[] args) {
-        long c = 600851475143l;
+        Long c = 600851475143l;
         System.out.println(getPrime(c));
     }
 
-    public static long getPrime(long l) {
-        long prime = 1l;
+    public static long getPrime(long value) {
 
-        for (long i = 1; i < l; i++) {
+        for (long i = 2; i < value; i++) {
+            long remainder = value % i;
+            if (remainder == 0) {
+                long vv = value / i;
+                if (isPrime(vv)){
+                    return vv;
+                }else{
+                    return getPrime(vv);
+                }
 
-            if (i % 2 == 0 || i % 3 == 0 || i % 5 == 0 || i % 7 == 0 || i % 11 == 0
-                    || i % 13 == 0 || i % 17 == 0 || i % 19 == 0)
-                continue;
-
-            if (l % i == 0 && i > prime)
-                prime = i;
-
+            }
         }
-        return prime;
+        return 1;
     }
+
+    public static Boolean isPrime(long value) {
+        Boolean flag = true;
+        for (int i = 2; i <= Math.sqrt(value); i++) {
+            if (value % i == 0) {
+                flag = false;
+                break;
+            }
+        }
+        return flag;
+    }
+
+
 }
